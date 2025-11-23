08b81fd8a2d056dd92fa5b21c3d0e469f2ad99bf
lixxmaduh-maker <lixx.maduh@gmail.com>
2025-11-23 20:33:10 -0300

    feat: listagem de produtos funcionando na listagemVIEW

--- src/listagemVIEW.java
+++ src/listagemVIEW.java
@@ -181,10 +181,8 @@ public class listagemVIEW extends javax.swing.JFrame {
         //</editor-fold>
 
         /* Create and display the form */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new listagemVIEW().setVisible(true);
-            }
+        java.awt.EventQueue.invokeLater(() -> {
+            new listagemVIEW().setVisible(true);
         });
     }
 
