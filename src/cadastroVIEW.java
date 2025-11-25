9651b497efb9660b26b56c78cd442a7234cbad7e
lixxmaduh-maker <lixx.maduh@gmail.com>
2025-11-24 21:26:23 -0300

    feat: botão Consultar vendas abrindo tela vendasVIEW

--- src/cadastroVIEW.java
+++ src/cadastroVIEW.java
@@ -1,3 +1,6 @@
+
+import java.awt.HeadlessException;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
@@ -186,7 +189,7 @@ public class cadastroVIEW extends javax.swing.JFrame {
                 javax.swing.JOptionPane.ERROR_MESSAGE);
         }
 
-    } catch (Exception ex) {
+    } catch (HeadlessException ex) {
         javax.swing.JOptionPane.showMessageDialog(this, 
             "Erro ao salvar: " + ex.getMessage(), 
             "Erro", 
